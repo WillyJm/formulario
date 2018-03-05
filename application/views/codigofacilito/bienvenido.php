289ebc722854e3800989cf53ed0418ed9399d708
william medina <williamc0608@gmail.com>
2018-03-05 13:41:32 -0500

    Primer formulario

---
+++ application/views/codigofacilito/bienvenido.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+	<title>hola</title>
+</head>
+<body>
+	<h1>Llamado desde el controlador Código facilito</h1>
+</body>
+</html>
---
+++ application/views/codigofacilito/headers.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<<html lang='es'>
+<head>
+	<title>CodeIngniter curso</title>
+	<meta charset='utf-8'>
+</head>
