256ed3e7b7b5762776a6cf4047183709048b6646
william medina <williamc0608@gmail.com>
2018-03-12 12:00:37 -0500

    report3

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+
+
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'maplestory',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
